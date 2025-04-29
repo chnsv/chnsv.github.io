52c323f118bb977cbc5c9ac37fa430cb93eebdb9
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2025-04-29 17:46:54 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
+// server.js
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const sequelize = require('./config/db');
-const authRoutes = require('./routes/AuthRoutes');
-const courseRoutes = require('./routes/CourseRoutes');
-const applicationRoutes = require('./routes/ApplicationRoutes');
 
-const app = express();
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Подключение маршрутов
-app.use('/api/auth', authRoutes);
-app.use('/api/courses', courseRoutes);
-app.use('/api/applications', applicationRoutes);
+// Routes
+app.use('/api/auth', require('./routes/AuthRoutes'));
 
-// Синхронизация с БД и запуск сервера
-sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log('Сервер запущен на порту 3001');
+// Подключение к БД и запуск сервера
+sequelize.sync()
+  .then(() => {
+    app.listen(3001, () => {
+      console.log('Сервер запущен на порту 3001');
+    });
+  })
+  .catch(err => {
+    console.error('Ошибка подключения к БД:', err);
   });
-});
