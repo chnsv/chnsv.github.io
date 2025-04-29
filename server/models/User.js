6fbaa7decfca52ff223843077712bc80f5292f84
Chernyshova Nika <143995178+chnsv@users.noreply.github.com>
2025-04-29 16:28:33 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,19 @@
-const { DataTypes } = require('sequelize');
-const db = require('../config/db');
-const bcrypt = require('bcryptjs');
-
-const User = db.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: 'user'
     }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  role: {
-    type: DataTypes.ENUM('student', 'teacher', 'admin'),
-    defaultValue: 'student'
-  }
-}, {
-  hooks: {
-    beforeCreate: async (user) => {
-      user.password = await bcrypt.hash(user.password, 10);
-    }
-  }
-});
+  });
 
-User.prototype.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+  return User;
 };
-
-module.exports = User;
